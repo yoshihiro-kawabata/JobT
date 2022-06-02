cd951eebb6945757f7eeb1942aa0e4c7cca9977e
yoshihiro_kawabata <yoshihiro_kawabata@mediado.jp>
2022-06-02 13:08:11 +0900

    jobt開発_改修

--- db/seeds.rb
+++ db/seeds.rb
@@ -248,9 +248,10 @@ numbA = 0
   )
 
   numbA = "#{n}".to_i
-  @attendance = Attendance.find_by(attendance_date:start_day, user_id: userA.id)
-  if numbA % 4 != 0
-  @attendance.update(start_time: start_timeA.strftime("%H:%M"))
+
+  unless userA.admin? 
+    @attendance = Attendance.find_by(attendance_date:start_day, user_id: userA.id)
+    @attendance.update(start_time: start_timeA.strftime("%H:%M"))
   end
   if numbA % 2 != 0 
     @attendance.update(start_time: start_timeA.strftime("%H:%M"), end_time: end_timeA.strftime("%H:%M"))
