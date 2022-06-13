ebe8cd254655a7b97c5f95e7a094c38a06be7e44
yoshihiro_kawabata <yoshihiro_kawabata@mediado.jp>
2022-06-13 10:03:49 +0900

    jobt開発_改修

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     attr_accessor :remember_token
-    validates :number, presence: true, length: { maximum: 30 }
+    validates :number, presence: true, length: { maximum: 30 }, numericality: {only_integer: true, greater_than_or_equal_to: 0}
     validates :name, presence: true, length: { maximum: 30 }
     validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     before_validation { email.downcase! }
